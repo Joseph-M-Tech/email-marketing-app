e9195f7ece0b9f7201f1faf54dc895308407886b
unknown <citytrotkenya@gmail.com>
2025-09-08 19:35:36 +0300

    update dependencies

---
+++ src/main/java/com/example/Landing/LandingEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.landing;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LandingEmailApplication {
+public static void main(String[] args) {
+SpringApplication.run(LandingEmailApplication.class, args);
+}
+}
